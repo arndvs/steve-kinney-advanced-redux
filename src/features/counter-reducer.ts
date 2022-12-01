7f964e1710c5a37f00a84111e502c21af2ff1ff5
Aaron Davis <aaron@ripemetrics.com>
2022-12-01 14:47:53 -0800

    count reducer solution

--- src/features/counter-reducer.ts
+++ src/features/counter-reducer.ts
@@ -1,4 +1,4 @@
-import { createAction } from '@reduxjs/toolkit'; // createAction is a helper function for defining a Redux action type and creator
+import {  createAction, createReducer } from '@reduxjs/toolkit'; // createAction is a helper function for defining a Redux action type and creator
 
 type CounterState = { count: number }; // this is the type of the state - it's an object with a count property that is a number
 
@@ -17,6 +17,24 @@ type CounterState = { count: number }; // this is the type of the state - it's a
   | ReturnType<typeof increment> // ReturnType is a type that takes a function type as an argument and returns the return type of the function
   | ReturnType<typeof decrement>
   | ReturnType<typeof reset>;
+
+
+  export const counterReducer = createReducer({ count: 0 }, (builder) => { // createReducer takes two arguments - the initial state and a builder function. The builder function takes a builder object as an argument
+    builder.addCase(increment, (state, action) => { // builder.addCase takes two arguments - the action creator function and a reducer function. The reducer function takes the state and the action as arguments
+      state.count += action.payload; // the reducer function returns the new state - in this case, it increments the count by the amount in the action payload
+    });
+
+    builder.addCase(decrement, (state, action) => { // builder.addCase takes two arguments - the action creator function and a reducer function. The reducer function takes the state and the action as arguments
+      state.count -= action.payload; // the reducer function returns the new state - in this case, it decrements the count by the amount in the action payload
+    });
+
+    builder.addCase(reset, (state) => { //
+        state.count = 0;// the reducer function returns the new state - in this case, it resets the count to 0
+        }
+    );
+  });
+
+// the code above replaces the code below with better type safety, less boilerplate, and more efficient code
 
 export const reducer = (state: CounterState, action: CounterAction) => { // the reducer takes the state and the action as arguments and returns the new state
     if (action.type === increment.type) { // if the action type is the same as the increment action type
